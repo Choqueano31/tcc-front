1b82a4eb8532a38d8b16d898b9934efea9dfbac6
unknown <rafataell@gmail.com>
2022-10-15 18:08:21 -0300

    update service api

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const myApi = axios.create({
  //baseURL: "https://rafaelapi.herokuapp.com",
-  baseURL: "http://localhost:3333",
+ baseURL: "https://tcc-api.onrender.com",
+//  baseURL: "http://localhost:3333",
 });
 
 export default myApi;

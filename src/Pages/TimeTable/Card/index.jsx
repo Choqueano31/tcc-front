818db53f80cdd5f558bfa4cc1c4019653b48d6ef
unknown <rafataell@gmail.com>
2022-10-16 17:53:07 -0300

    l

--- src/Pages/TimeTable/Card/index.jsx
+++ src/Pages/TimeTable/Card/index.jsx
@@ -7,12 +7,70 @@ import { MdSettingsApplications } from 'react-icons/md';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
-import { Container, Label } from './styles';
+//import { Container, Label } from './styles';
 import BoardContext from '../Board/context';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.css';
 
+import styled, { css } from 'styled-components';
+
+ const Container = styled.div`
+  position: relative;
+  background: #FFF;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 15px;
+  height: 80px;
+  box-shadow: 0 1px 4px 0 rgba(192, 208, 230, 0.8);
+  border-top: 20px solid rgba(230, 236, 245, 0.4);
+  cursor: grab;
+  header {
+    position: absolute;
+    top: -25px;
+    left: 15px;
+
+  }
+
+  p {
+    font-weight: 500;
+    line-height: 20px;
+  }
+
+  img {
+    width: 24px;
+    height: 24px;
+    border-radius: 2px;
+    margin-top: 5px;
+  }
+  ${(props) => props.isDragging && css`
+    border: 2px dashed rgba(0,0,0,0.2);
+    padding-top: 31px;
+    border-radius: 0;
+    background: transparent;
+    box-shadow: none;
+    cursor: grabbing;
+    p,img,header {
+      opacity: 0;
+    }
+  `}
+  aside {
+    position: absolute;
+    top: 40px;
+    left: 140px;
+    cursor: pointer;
+
+  }
+`;
+
+ const Label = styled.span`
+  width: 10px;
+  height: 10px;
+  border-radius: 2px;
+  display: inline-block;
+  background: ${(props) => props.color};
+
+`;
 function Card({ data, index, listIndex }) {
   const ref = useRef();
   const [name, setName] = useState('');
